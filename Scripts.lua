e20d072be1d2da9a876f80bf9e4adf87dc4de0db
KelsHax <89037551+KelsHax@users.noreply.github.com>
2021-08-27 11:49:11 +0800

    Create Scripts.lua

---
+++ Scripts.lua
@@ -0,0 +1,28 @@
+if game.PlaceId == 1499872953 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/1499872953.lua"))();
+ elseif game.PlaceId == 2116452049 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/KryptonHub/main/script/obfuscate/Steve's_One_Piece.lua"))();
+  elseif game.PlaceId == 2281639237 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/KryptonHub/main/script/obfuscate/Stand_Online.lua"))();
+  elseif game.PlaceId == 394506555 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/394506555.lua"))();
+  elseif game.PlaceId == 4520749081 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/4520749081.lua"))();
+  elseif game.PlaceId == 5170554573 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/5170554573.lua"))();
+  elseif game.PlaceId == 6961830235 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/5303541547.lua"))();
+  elseif game.PlaceId == 5835263912 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/5835263912.lua"))();
+  elseif game.PlaceId == 6186867282 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6186867282.lua"))();
+  elseif game.PlaceId == 6329844902 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6329844902.lua"))();
+  elseif game.PlaceId == 6381829480 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6381829480.lua"))();
+  elseif game.PlaceId == 6574375780 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6574375780.lua"))();
+  elseif game.PlaceId == 4996049426 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/KryptonHub/main/script/obfuscate/All_Star_Tower_Defense.lua"))();
+  else return error("this game did't support!", 2)
+  end
